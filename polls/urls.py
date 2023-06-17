fcd2eeb143d8725bb0a16b2de06fac472b6505e8
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-06-17 23:41:02 +0700

    update

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('detail/<int:question_id>', views.detailView, name="detail"),
     path('list/', views.viewlist, name='view_list'),
     path('', views.index, name='index'),
 ]
